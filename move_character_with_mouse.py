9b1bc325a9d7a055d226e026568c9f5ed70f1a51
neneee0181 <2022180024@tukorea.ac.kr>
2024-10-03 13:25:54 +0900

    update: 마우스 위치에 따른 캐릭터 이미지 이동 완료

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,3 +1,4 @@
+import random
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -5,39 +6,52 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
+def moveImg():
+    global x, y
+    global img_position_x, img_position_y
+
+    if x < img_position_x:
+        img_position_x -= 1
+    elif x > img_position_x:
+        img_position_x += 1
+
+    if y < img_position_y:
+        img_position_y -= 1
+    elif y > img_position_y:
+        img_position_y += 1
 
 def handle_events():
     global running
     global x, y
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
 
-
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
-hide_cursor()
+img_position_x, img_position_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(x,y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, img_position_x, img_position_y)
     update_canvas()
     frame = (frame + 1) % 8
+    if (x == img_position_x and y == img_position_y):
+        x, y = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
+    else:
+        moveImg()
 
     handle_events()
 
 close_canvas()
-
-
-
-
